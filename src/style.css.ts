e57f1c8e67b1ee8b5000375d7629de2b4b0e0b90
Yashvanthk05 <yashvanthk05@gmail.com>
2025-06-27 13:00:21 +0530

    vanilla extract added

---
+++ src/style.css.ts
@@ -0,0 +1,16 @@
+import { createTheme,style } from "@vanilla-extract/css";
+
+export const [themeClass,vars]=createTheme({
+    color:{
+        primary: '#1e60ff',
+        secondary: '#3a8dff',
+        accent: '#5abcf5',
+        base: '#dedede',
+        text: '#1a1a1a',
+        muted: '#7a7a7a',
+    }
+});
+
+export const body=style({
+    
+})
